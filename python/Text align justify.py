4f67f036d537db431e19ccc2330f730112f7a939
Koen Ongena <koen.ongena@imec.be>
2021-10-21 15:46:07 +0200

    Finished text align in python

--- python/Text align justify.py
+++ python/Text align justify.py
@@ -1,8 +1,7 @@
-import math
-
-
 def justify_line(t, width):
-    print('Justifying', t)
+    if ' ' not in t:
+        return t
+
     words = t.split(' ')
     i = 0
     while sum([len(w) for w in words]) < width:
@@ -19,15 +18,20 @@ def justify(text, width):
         return text[0:width] + '\n' + justify(text[width:].strip(), width)
 
     last_space_index = text.rfind(' ', 0, width)
-    return justify_line(text[0: last_space_index].strip(), width) + '\n' + justify(text[last_space_index:].strip(), width)
+    return justify_line(text[0: last_space_index].strip(), width) + '\n' + justify(text[last_space_index:].strip(),
+                                                                                   width)
 
 
-if __name__ == '__main__':
-    # print(justify_line('a b c', 6))
-    # print(justify_line('Lorem ipsum dolor sit amet,', 30))
+def test_codewars():
+    assert justify('123 45 6', 7) == '123  45\n6'
 
-    print(justify('123 45 6', 7))
-    # print(justify('na de 4de keer', 5))
-    print(justify('''Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum sagittis dolor mauris, at elementum ligula tempor eget. In quis rhoncus nunc, at aliquet orci. Fusce at dolor sit amet felis suscipit tristique. Nam a imperdiet tellus. Nulla eu vestibulum urna. Vivamus tincidunt suscipit enim, nec ultrices nisi volutpat ac. Maecenas sit amet lacinia arcu, non dictum justo. Donec sed quam vel risus faucibus euismod. Suspendisse rhoncus rhoncus felis at fermentum. Donec lorem magna, ultricies a nunc sit amet, blandit fringilla nunc. In vestibulum velit ac felis rhoncus pellentesque. Mauris at tellus enim. Aliquam eleifend tempus dapibus. Pellentesque commodo, nisi sit amet hendrerit fringilla, ante odio porta lacus, ut elementum justo nulla et dolor.
-dolor sit amet, consectetur adipiscing elit. Vestibulum sagittis dolor mauris, at elementum ligula tempor eget. In quis rhoncus nunc, at aliquet orci. Fusce at dolor sit amet felis suscipit tristique. Nam a imperdiet tellus. Nulla eu vestibulum urna. Vivamus tincidunt suscipit enim, nec ultrices nisi volutpat ac. Maecenas sit amet lacinia arcu, non dictum justo. Donec sed quam vel risus faucibus euismod. Suspendisse rhoncus rhoncus felis at fermentum. Donec lorem magna, ultricies a nunc sit amet, blandit fringilla nunc. In vestibulum velit ac felis rhoncus pellentesque. Mauris at tellus enim. Aliquam eleifend tempus dapibus. Pellentesque commodo, nisi sit amet hendrerit fringilla, ante odio porta lacus, ut elementum justo nulla et dolor.
-consectetur adipiscing elit. Vestibulum sagittis dolor mauris, at elementum ligula tempor eget. In quis rhoncus nunc, at aliquet orci. Fusce at dolor sit amet felis suscipit tristique. Nam a imperdiet tellus. Nulla eu vestibulum urna. Vivamus tincidunt suscipit enim, nec ultrices nisi volutpat ac. Maecenas sit amet lacinia arcu, non dictum justo. Donec sed quam vel risus faucibus euismod. Suspendisse rhoncus rhoncus felis at fermentum. Donec lorem magna, ultricies a nunc sit amet, blandit fringilla nunc. In vestibulum velit ac felis rhoncus pellentesque. Mauris at tellus enim. Aliquam eleifend tempus dapibus. Pellentesque commodo, nisi sit amet hendrerit fringilla, ante odio porta lacus, ut elementum justo nulla et dolor.''', 15))
+
+def test_simple():
+    assert justify('Lorem ipsum dolor', 15) == 'Lorem     ipsum\ndolor'
+    assert justify('Lorem ipsum dolor sit amet, consectetur', 15) == 'Lorem     ipsum\ndolor sit amet,\nconsectetur'
+
+
+def test_single_word_on_line():
+    given = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit'
+    expected = 'Lorem     ipsum\ndolor sit amet,\nconsectetur\nadipiscing elit'
+    assert justify(given, 15) == expected

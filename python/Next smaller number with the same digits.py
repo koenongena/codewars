bfd776b2bb4283f815f756ad515ca129c2f921a1
koen.ongena <gijoke@gmail.com>
2021-11-20 20:08:42 +0100

    Next smaller number, "Lexicographically previous permutation" algorithm.

--- python/Next smaller number with the same digits.py
+++ python/Next smaller number with the same digits.py
@@ -1,41 +1,16 @@
-def swap(arr):
-    return [arr[1], arr[0]]
-
-
-def extract_digits(n):
-    return [int(d) for d in str(n)]
-
-def greatest_number_from_digits(digits):
-    return arr_to_number(sorted(digits, reverse=True))
-
 def next_smaller(n):
-    def next_smaller_in_digits(digits):
-        if len(digits) == 1:
-            return []
-        elif len(digits) == 2:
-            return swap(digits) if digits[0] > digits[1] else []
+    digits = list(str(n))
 
-        sub_part = next_smaller_in_digits(digits[1:])
-        if sub_part:
-            return [digits[0]] + sub_part
+    i = len(digits) - 1
+    while i > 0 and digits[i - 1] <= digits[i]: i -= 1
+    if i <= 0: return -1
 
-        # Maybe the first digits is the one to change?
-        # So, find a digits small than the first digit, but not 0
-        smaller_than_first_digit = [d for d in digits[1:] if d < digits[0]]
-        if smaller_than_first_digit:
-            largest_of_the_smaller = sorted(smaller_than_first_digit, reverse=True)[0]
-            remaining_digits = digits[:]
-            remaining_digits.remove(largest_of_the_smaller)
-            return [largest_of_the_smaller] + sorted(remaining_digits, reverse=True)
-
-        return []
-
-    dn = next_smaller_in_digits(extract_digits(n))
-    return arr_to_number(dn) if dn and dn[0] != 0 else -1
-
-def arr_to_number(digits):
-    return int(''.join([str(d) for d in digits]))
-
+    j = len(digits) - 1
+    while digits[j] >= digits[i - 1]: j -= 1
+    digits[i - 1], digits[j] = digits[j], digits[i - 1]
+    digits[i:] = reversed(digits[i:])
+    if digits[0] == '0': return -1
+    return int(''.join(digits))
 
 def test_codewars():
     assert next_smaller(9) == -1

fc70bb5bee2b34ce923a679ad0b91d27ee354dc0
koen.ongena <gijoke@gmail.com>
2021-11-20 17:04:10 +0100

    Next smaller number, my implementation. Apparently this can be solved using the "Lexicographically previous permutation" algorithm. I'll try that now...

--- python/Next smaller number with the same digits.py
+++ python/Next smaller number with the same digits.py
@@ -2,48 +2,46 @@ def swap(arr):
     return [arr[1], arr[0]]
 
 
+def extract_digits(n):
+    return [int(d) for d in str(n)]
+
+def greatest_number_from_digits(digits):
+    return arr_to_number(sorted(digits, reverse=True))
+
 def next_smaller(n):
-    def find_successors_including_zero(digits):
-        return sorted([d for d in digits[1:] if d < digits[0]], reverse=True)
-
-    def find_successors_without_zero(digits):
-        return sorted([d for d in digits[1:] if d < digits[0] and d != 0], reverse=True)
-
-    def _next_smaller(digits, find_successors=find_successors_without_zero):
-        if len(digits) < 2:
+    def next_smaller_in_digits(digits):
+        if len(digits) == 1:
             return []
         elif len(digits) == 2:
             return swap(digits) if digits[0] > digits[1] else []
 
-        # Keep the first digit and determine the next smaller number for the rest of the digits
-        without_first_digit = _next_smaller(digits[1:], find_successors_including_zero)
-        if without_first_digit:
-            return [digits[0]] + without_first_digit
-        # if there is not next number, try to replace the first digit with a smaller number
-        successors = find_successors(digits)
-        if successors:
-            digits_ = (digits[1:])
-            digits_.remove(successors[0])
-            digits_.append(digits[0])
-            digits_.sort(reverse=True)
+        sub_part = next_smaller_in_digits(digits[1:])
+        if sub_part:
+            return [digits[0]] + sub_part
 
-            return [successors[0]] + digits_
+        # Maybe the first digits is the one to change?
+        # So, find a digits small than the first digit, but not 0
+        smaller_than_first_digit = [d for d in digits[1:] if d < digits[0]]
+        if smaller_than_first_digit:
+            largest_of_the_smaller = sorted(smaller_than_first_digit, reverse=True)[0]
+            remaining_digits = digits[:]
+            remaining_digits.remove(largest_of_the_smaller)
+            return [largest_of_the_smaller] + sorted(remaining_digits, reverse=True)
 
         return []
 
-    result = _next_smaller(extract_digits(n), find_successors_without_zero)
-    return arr_to_number(result) if result else -1
-
-
-def extract_digits(n):
-    return [int(d) for d in str(n)]
-
+    dn = next_smaller_in_digits(extract_digits(n))
+    return arr_to_number(dn) if dn and dn[0] != 0 else -1
 
 def arr_to_number(digits):
     return int(''.join([str(d) for d in digits]))
 
 
 def test_codewars():
+    assert next_smaller(9) == -1
+    assert next_smaller(10) == -1
+    assert next_smaller(12) == -1
+    assert next_smaller(21) == 12
     assert next_smaller(907) == 790
     assert next_smaller(531) == 513
     assert next_smaller(135) == -1
@@ -52,7 +50,9 @@ def test_codewars():
     assert next_smaller(123456798) == 123456789
     assert next_smaller(123456789) == -1
     assert next_smaller(1234567908) == 1234567890
-    assert next_smaller(219) == 129
+    assert next_smaller(219) == 192
+    assert next_smaller(220) == 202
+    assert next_smaller(2013489474370) == 2013489474307
 
 
 def test_codewars_attempt():
